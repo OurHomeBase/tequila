b5032920f809de29e23a05c5e158e1a71feb7ae3
Yury Agasyants <yury@Yurys-MacBook-Pro.local>
2015-11-05 18:02:20 -0800

    Fixed lint errors in run_unit_tests.

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -1,5 +1,12 @@
+'''A module to run unit tests.'''
+
 import unittest
 
+def _run_tests():
+  test_suite = unittest.TestLoader().discover(
+      start_dir='unit_tests', pattern='*_test.py', top_level_dir='.')
+
+  unittest.TextTestRunner(verbosity=2).run(test_suite)
+
 if __name__ == "__main__":
-  suite = unittest.TestLoader().discover(start_dir='unit_tests', pattern='*_test.py', top_level_dir='.')
-  unittest.TextTestRunner(verbosity=2).run(suite)
+  _run_tests()
