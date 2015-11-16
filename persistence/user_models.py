13499e697037c6c846e616f634dfdaf04b990783
Janet Zhou <JanetZhou@Xuejins-MacBook-Pro.local>
2015-11-15 21:24:10 -0800

    Added Addresses, TimeProfile, and email to User model. Added tests.

--- persistence/user_models.py
+++ persistence/user_models.py
@@ -2,11 +2,42 @@
 
 from google.appengine.ext import ndb
 from persistence import persistence_utils
+from google.appengine.api import datastore_errors
 
+_ADDR_DEFAULT_TYPE = 'home'
+_ACCOUNT_STATUS_CHOICES = [1, 2] #1=active, 2=locked
+_WORK_STATUS_CHOICES = ['Full Time', 'Part Time', 'None']
+
+class TimeProfile(ndb.Model):
+  '''Stores user defined time profile information.'''
+  def week_validator(self, value):
+    if not value <= 7*24:
+      raise datastore_errors.BadValueError('expect value <= 168, got %s.' % repr(value))
+
+  hasChildren = ndb.BooleanProperty()
+  workStatus = ndb.StringProperty(choices=_WORK_STATUS_CHOICES)
+  choreHrs = ndb.FloatProperty(validator=week_validator)
+
+class Address(ndb.Model):
+  '''Stores user's address'''
+  addr_type = ndb.StringProperty(default=_ADDR_DEFAULT_TYPE)
+  street = ndb.StringProperty()
+  city = ndb.StringProperty()
+  country = ndb.StringProperty()
+  zip = ndb.StringProperty()
 
 class User(ndb.Model):
   '''Stores basic user properties.'''
   username = ndb.StringProperty()
+  email = ndb.StringProperty()
+  emailVerified = ndb.BooleanProperty()
+  name = ndb.StringProperty()
+
+  time_profile = ndb.StructuredProperty(TimeProfile)
+  addresses = ndb.StructuredProperty(Address, repeated=True)
+
+  lastLogin = ndb.DateTimeProperty()
+  accountStatus = ndb.IntegerProperty(choices=_ACCOUNT_STATUS_CHOICES, default=1)
 
   @classmethod
   def find_by_id(cls, key_id):
@@ -19,4 +50,16 @@ class User(ndb.Model):
     query = User.query(User.username == username)
 
     return persistence_utils.fetch_first_or_none(query)
+
+  @classmethod
+  def find_by_email(cls, email):
+    list_users = User.query(User.email == email).fetch(1)
+
+    return list_users[0] if list_users else None
+
+  def find_address_by_type(self, addr_type=_ADDR_DEFAULT_TYPE):
+    for addr in self.addresses:
+      if addr.addr_type == addr_type:
+        return addr
+    return None
 
--- unit_tests/persistence/user_models_test.py
+++ unit_tests/persistence/user_models_test.py
@@ -4,6 +4,7 @@ import unittest
 
 from persistence import user_models
 from unit_tests.common import test_utils
+from google.appengine.api import datastore_errors
 
 # pylint: disable=missing-docstring
 class UserTest(test_utils.CommonNdbTest):
@@ -32,6 +33,61 @@ class UserTest(test_utils.CommonNdbTest):
     # Verify
     self.assertTrue(found_user)
     self.assertEqual('my_user', found_user.username)
+
+  def test_find_by_email_returns_expected_user(self):
+    # Setup.
+    created_user = user_models.User(email='my_user@gmail.com')
+    created_user.put()
+
+    # Exercise.
+    found_user = user_models.User.find_by_email('my_user@gmail.com')
+
+    # Verify
+    self.assertTrue(found_user)
+    self.assertEqual('my_user@gmail.com', found_user.email)
+
+  def test_find_address_by_type_returns_expected_address(self):
+    # Setup.
+    created_user_no_address = user_models.User(email='no_address@gmail.com')
+    created_user_no_address.put()
+
+    created_user_one_address = user_models.User(email='one_address@gmail.com')
+    created_user_one_address.addresses = [user_models.Address(city='San Jose')]
+    created_user_one_address.put()
+
+    created_user_multi_addresses = user_models.User(email='multi_addresses@gmail.com')
+    addr1 = user_models.Address(addr_type='work', city='Amsterdam')
+    addr2 = user_models.Address(addr_type='home', city='Campbell')
+    created_user_multi_addresses.addresses = [addr1, addr2]
+    created_user_multi_addresses.put()
+
+    # Exercise.
+    found_user_no_addr = user_models.User.find_by_email('no_address@gmail.com')
+    found_user_one_addr = user_models.User.find_by_email('one_address@gmail.com')
+    found_user_multi_addr = user_models.User.find_by_email('multi_addresses@gmail.com')
+
+    # Verify
+    self.assertTrue(found_user_no_addr)
+    self.assertIsNone(found_user_no_addr.find_address_by_type())
+
+    self.assertTrue(found_user_one_addr)
+    self.assertEqual('San Jose', found_user_one_addr.find_address_by_type().city)
+
+    self.assertTrue(found_user_multi_addr)
+    self.assertEqual('Amsterdam', found_user_multi_addr.find_address_by_type('work').city)
+    self.assertEqual('Campbell', found_user_multi_addr.find_address_by_type('home').city)
+
+  def test_time_profile_validations_and_defaults(self):
+    # Setup.
+    # Exercise.
+    user_models.TimeProfile(hasChildren=True, workStatus='Full Time', choreHrs=20)
+
+    # Verify
+    with self.assertRaises(datastore_errors.BadValueError):
+      user_models.TimeProfile(hasChildren=True, workStatus='nonsense', choreHrs=20)
+
+    with self.assertRaises(datastore_errors.BadValueError):
+      user_models.TimeProfile(hasChildren=True, workStatus='Full Time', choreHrs=170)
 
 if __name__ == "__main__":
   unittest.main()
